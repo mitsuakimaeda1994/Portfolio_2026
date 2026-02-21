70d13921211bb830ebf8a9014542fe5f6ab628a4
mitsu <mitsuakisince1994@gmail.com>
2026-02-21 18:29:19 +0900

    各種調整

--- data.js
+++ data.js
@@ -4,44 +4,44 @@
 window.WORKS = [
   {
     title: "Global Hit Maker(NHK)",
-    desc: "撮影 / 画作り / 照明。実写の質感と設計をまとめたリール。",
+    desc: "Videographer",
     youtubeId: "AOrnsHqWgLE?si=xEywWoIahEWn3E1_",
-    tags: ["Cinematography", "On-set", "Lighting"]
+    tags: ["Videographer"]
   },
   {
     title: "Replay Project-Drama (au)",
-    desc: "実写×生成のVFX。置換・拡張・クリーンアップ・合成の事例。",
+    desc: "Videographer",
     youtubeId: "dz65VPfQbxQ?si=6PcYNAOYaWogZVAw",
-    tags: ["AI VFX", "Comp", "GenAI"]
+    tags: ["Videographer"]
   },
   {
     title: "YOKAI:YAROKAMIZU(NHK World)",
-    desc: "UE5でのプレビズ。カメラ・ルック・ライティング検証。",
+    desc: "Videographer",
     youtubeId: "FLZVzfMqqD4?si=emfZRd9sk8ALdEZu",
-    tags: ["UE5", "Previz", "Sequencer"]
+    tags: ["Videographer"]
   },
   {
     title: "社内PV-konika(stu)",
-    desc: "UE5でのプレビズ。カメラ・ルック・ライティング検証。",
+    desc: "Videographer",
     youtubeId: "b4G3XYhJrl4?si=ztuWL5ovVQnWroqW",
-    tags: ["UE5", "Previz", "Sequencer"]
+    tags: ["Videographer"]
   },
   {
     title: "社内PV-YAMAHA(stu)",
-    desc: "UE5でのプレビズ。カメラ・ルック・ライティング検証。",
+    desc: "Videographer",
     youtubeId: "te5t7_c2OxU?si=kGuYENw8HXthPZoj",
-    tags: ["UE5", "Previz", "Sequencer"]
+    tags: ["Videographer"]
   },
   {
     title: "社内PV(SRTA)",
-    desc: "UE5でのプレビズ。カメラ・ルック・ライティング検証。",
+    desc: "AI Artist",
     youtubeId: "LMSIM4CIqjk?si=8xg6WqwxZ_sbO0AZ",
-    tags: ["UE5", "Previz", "Sequencer"]
+    tags: ["AI Artist"]
   },
   {
     title: "Replay Project-Cover Artist (au)",
-    desc: "UE5でのプレビズ。カメラ・ルック・ライティング検証。",
+    desc: "Videographer",
     youtubeId: "mjZO5J-o9EM?si=JkorCKLlp3zlAUZ5",
-    tags: ["UE5", "Previz", "Sequencer"]
+    tags: ["Videographer"]
   }
 ];
